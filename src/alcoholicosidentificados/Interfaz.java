658a3e4533035100ca6651c335720b01f89daf05
AnabelGRios <anabelgrios@gmail.com>
2015-02-20 15:56:48 +0100

    Añadido el tipo enumerado Interfaz

---
+++ src/alcoholicosidentificados/Interfaz.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ddsi;
+
+/**
+ *
+ * @authors
+ *          Anabel Gómez Ríos
+ *          Gustavo Rivas Gervilla
+ *          Luis Suárez Lloréns
+ *          Jacinto Carrasco Castillo
+ */
+public enum Interfaz {
+    I_ADMINISTRADOR,I_CONSUMIDOR,I_EMPRESA
+}
